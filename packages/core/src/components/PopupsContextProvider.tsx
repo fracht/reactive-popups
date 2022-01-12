44e8543be1607d06ee3ff0bb22a4d8053c73a1c4
Aleksandras Šukelovič <alex.shukel@gmail.com>
2022-01-12 19:41:06 +0200

    Added PopupsWrapper option

--- packages/core/src/components/PopupsContextProvider.tsx
+++ packages/core/src/components/PopupsContextProvider.tsx
@@ -1,23 +1,28 @@
-import React, { PropsWithChildren } from 'react';
+import React, { ComponentType, PropsWithChildren } from 'react';
 
 import { usePopupsBag } from '../hooks/usePopupsBag';
 import { PopupsContext } from '../PopupsContext';
 
-type PopupsContextProviderType = PropsWithChildren<{}>;
+type PopupsContextProviderType = PropsWithChildren<{
+    PopupsWrapper?: ComponentType<PropsWithChildren<{}>>;
+}>;
 
 export const PopupsContextProvider = ({
     children,
+    PopupsWrapper = React.Fragment,
 }: PopupsContextProviderType) => {
     const { popups, visiblePopups, ...context } = usePopupsBag();
 
     return (
         <PopupsContext.Provider value={context}>
             {children}
-            {popups
-                .filter((popup) => visiblePopups.has(popup.id))
-                .map(({ PopupComponent, props, id }) => {
-                    return <PopupComponent {...props} key={id} id={id} />;
-                })}
+            <PopupsWrapper>
+                {popups
+                    .filter((popup) => visiblePopups.has(popup.id))
+                    .map(({ PopupComponent, props, id }) => {
+                        return <PopupComponent {...props} key={id} id={id} />;
+                    })}
+            </PopupsWrapper>
         </PopupsContext.Provider>
     );
 };
