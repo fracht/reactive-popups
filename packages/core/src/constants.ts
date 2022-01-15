e52c2f97b150b4484e484bce8002c3af6a0f8179
Aleksandras Šukelovič <alex.shukel@gmail.com>
2022-01-15 15:16:11 +0200

    Created CONTEXT_NOT_FOUND_ERROR constant

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,1 +1,3 @@
 export const INITIAL_ID = 1;
+export const CONTEXT_NOT_FOUND_ERROR =
+    'PopupsContext was not found. Please, wrap your application with PopupsContextProvider.';
--- packages/core/src/hooks/usePopupsRegistryContext.ts
+++ packages/core/src/hooks/usePopupsRegistryContext.ts
@@ -2,16 +2,14 @@ import { useContext } from 'react';
 import { Stock } from 'stocked';
 import invariant from 'tiny-invariant';
 
+import { CONTEXT_NOT_FOUND_ERROR } from '../constants';
 import { PopupsRegistryContext } from '../PopupsRegistryContext';
 import { PopupsRegistry } from '../types/PopupsRegistry';
 
 export const usePopupsRegistryContext = (): Stock<PopupsRegistry> => {
     const context = useContext(PopupsRegistryContext);
 
-    invariant(
-        context,
-        'PopupsContext was not found. Please, wrap your application with PopupsContextProvider.'
-    );
+    invariant(context, CONTEXT_NOT_FOUND_ERROR);
 
     return context;
 };
