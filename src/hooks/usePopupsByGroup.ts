44c70dbec43ae3d4e4533d5d6f84e59fdc85f283
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-06-16 08:29:40 +0300

    Fixed type declaration

--- src/hooks/usePopupsByGroup.ts
+++ src/hooks/usePopupsByGroup.ts
@@ -1,7 +1,8 @@
 import { usePopupsContext } from './usePopupsContext';
 import { PopupGroup } from '../components/PopupGroup';
+import { Popup } from '../types/Popup';
 
-export const usePopupsByGroup = (group: PopupGroup) => {
+export const usePopupsByGroup = (group: PopupGroup): Popup<unknown>[] => {
     const { getPopupsByGroup } = usePopupsContext();
     return getPopupsByGroup(group);
 };
