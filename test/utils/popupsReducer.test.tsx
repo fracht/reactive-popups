601ff409b6ad4bbbc7bcc99de1be3cf7d11a43b7
sirse <artiom.tretjakovas2@gmail.com>
2023-06-02 12:57:55 +0300

    Switch to cryptographically insecure random id generation

--- test/utils/popupsReducer.test.tsx
+++ test/utils/popupsReducer.test.tsx
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react';
 import { act, renderHook } from '@testing-library/react';
-import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid/non-secure';
 
 import { createPopupGroup } from '../../src/components/PopupGroup';
 import { DefaultPopup } from '../../src/types/DefaultPopup';
